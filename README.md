3284e484d936f5996e66863b4469bc08fffd766e
graham <graham@hexnet.org>
2022-11-14 11:48:56 -0500

    Final fixes for publishing
    - Remove other regex stuff that was causing problems with commented-out code lines
    - Remove regex stuff that deplicated the line-trimming stuff
    - Add `addDependency()` to ensure reloading on changes to included files

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
-# WebGL Loader for Webpack
+# Some WebGL ES loader for Webpack
 
-A loader for WebGL files that need to be included in your Webpack project. Allows code splitting via the custom `#include` directive, and does some basic optimisations.
+A Webpack loader for GLSL ES shaders based on James Armitage's [webgl-loader](https://gitlab.com/jla-/webgl-loader), published under the MIT license. Much of this README is copied verbatim from there.
+
+Allows code reuse via the custom `#include` directive.
 
 ## Usage
 
-In `webpack.config.js` 
+### In `webpack.config.js`:
 
 ```js
 ...
 module: {
     rules: [ {
-        test: /\.(frag|vert)/,
+        test: /\.(fs|vs|frag|vert)/,
         exclude: /node_modules/,
-        use: [ {
-            loader: 'webgl-loader'
-        } ]
+        loader: 'simple-webgl-loader'
     } ]
 },
 ....
 ```
 
-If your main shader function is in the file `main.frag` and you have a function in another file called `parallax_uv.frag` then your shader code would look like:
+### In your shader files:
 
-main.frag:
+The `#include ...` directive is not scoped &mdash; it simply takes the contents of the file referred to, parses it (allowing for nested includes), and dumps the result at the current line. Do not use quotes around the filename, and as with any preprocessor directive, do not add a semicolon.
+
+If your main function is in `main.fs`, and you have a function in another file called `parallax_uv.fs`, then your shader code would look like this:
+
+#### main.frag:
 
 ```
 precision highp float;
@@ -35,7 +39,7 @@ void main()
 }
 ```
 
-parallax_uv.frag:
+#### parallax_uv.frag:
 
 ```
 vec2 parallax_uv( vec2 uv, vec3 view_dir )
@@ -45,11 +49,7 @@ vec2 parallax_uv( vec2 uv, vec3 view_dir )
 }
 ```
 
-The `#include ...` directive is not scoped at all. It simply takes the contents of the file referred to, parses its contents (allowing for nested includes) and then dumps the result at the current line.
-
-Of course, if you have all your shader code in the one file then you don't need to use `#include`.
-
-Then, in your JavaScript:
+### In your JavaScript:
 
 ```js
 import fragSrc from '../shaders/main.frag'
@@ -69,13 +69,11 @@ if ( !gl.getShaderParameter( frag, gl.COMPILE_STATUS ) ) {
 
 The loader performs some basic optimisations, and returns the shader code as a single string ready for WebGL compilation. The process boils down to:
 
- - `#include` directive files are merged.
- - Unnecessary whitespace is collapsed.
- - Comments are removed.
- - Numbers of the form `1.0` are converted to `1.`.
- - Semi-colons are automatically added/removed from line endings where appropriate.
+ - `#include` directive files are merged
+ - Unnecessary whitespace is collapsed
+ - Comments are removed
 
-For example, the following shader code would be compiled to:
+For example, the following shader code would be compiled from:
 
 ```
 #ifdef GL_ES
@@ -99,6 +97,8 @@ void main() {
 }
 
 ```
+
+to:
 
 ```js
 "#ifdef GL_ES\nprecision highp float;\n#endif\nuniform vec3 lightDirection;\nuniform float ambientLight;\nuniform sampler2D albedo;\nvarying vec3 vNormal;\nvarying vec2 vUv;\nvoid main(){\nvec3 gray=vec3(0.4,0.4,0.4);\nvec3 tAlbedo=texture2D(albedo,vUv).rgb;\nfloat lightness=-clamp(dot(normalize(vNormal),normalize(lightDirection)),-1.,0.);\nlightness=ambientLight+(1.-ambientLight)*lightness;\ngl_FragColor=vec4(tAlbedo*lightness,1.);\n}"
--- index.js
+++ index.js
@@ -1,91 +1,72 @@
-const path = require( 'path' ),
-    fs = require( 'fs' )
+const fs = require('fs');
+const path = require('path');
 
-function parse( source, p ) {
-    // UNIX line endings
-    source = source.replace( /\r/g, '\n' )
-
+module.exports = function(source) {
+  const parse = (source, resourcePath) => {
+    // Use UNIX line endings
+    source = source.replace(/\r\n?/g, '\n');
+  
     // Remove all comments
-    source = source.replace( /\/\*[\s\S]*?\*\/|\/\/.*/g, '\n' )
-
-    // Collapse whitespace with newlines into one newline
-    source = source.replace( /(\s*\n\s*)+/g, '\n' )
-
-    // truncate trailing decimal 0s
-    source = source.replace( /\.0+(?![1-9])/g, '.' )
-
+    source = source.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '\n');
+  
     // Remove spaces around the following operators
-    source = source.replace( /\s*=\s*/g, '=' )
-    source = source.replace( /\s*<\s*/g, '<' )
-    source = source.replace( /\s*>\s*/g, '>' )
-    source = source.replace( /\s*<=\s*/g, '<=' )
-    source = source.replace( /\s*>=\s*/g, '>=' )
-    source = source.replace( /\s*==\s*/g, '==' )
-    source = source.replace( /\s*!=\s*/g, '!=' )
-    source = source.replace( /\s*\|\|\s*/g, '||' )
-    source = source.replace( /\s*\&\&\s*/g, '&&' )
-    source = source.replace( /\s*\^\^\s*/g, '^^' )
-    source = source.replace( /\s*\?\s*/g, '?' )
-    source = source.replace( /\s*\:\s*/g, ':' )
-    source = source.replace( /\s*\+=\s*/g, '+=' )
-    source = source.replace( /\s*\-=\s*/g, '-=' )
-    source = source.replace( /\s*\*=\s*/g, '*=' )
-    source = source.replace( /\s*\/=\s*/g, '/=' )
-    source = source.replace( /\s*\+\s*/g, '+' )
-    source = source.replace( /\s*\-\s*/g, '-' )
-    source = source.replace( /\s*\/\s*/g, '/' )
-    source = source.replace( /\s*\*\s*/g, '*' )
-    source = source.replace( /\s*\(\s*/g, '(' )
-    source = source.replace( /\s*\)\s*/g, ')' )
-    source = source.replace( /\s*\[\s*/g, '[' )
-    source = source.replace( /\s*\]\s*/g, ']' )
-    source = source.replace( /\s*!\s*/g, '!' )
-    source = source.replace( /\s*\+\+\s*/g, '++' )
-    source = source.replace( /\s*\-\-\s*/g, '--' )
-    source = source.replace( /\s*,\s*/g, ',' )
-    source = source.replace( /\s*;\s*/g, ';\n' )
+    source = source.replace(/\s*=\s*/g, '=');
+    source = source.replace(/\s*<\s*/g, '<');
+    source = source.replace(/\s*>\s*/g, '>');
+    source = source.replace(/\s*<=\s*/g, '<=');
+    source = source.replace(/\s*>=\s*/g, '>=');
+    source = source.replace(/\s*==\s*/g, '==');
+    source = source.replace(/\s*!=\s*/g, '!=');
+    source = source.replace(/\s*\|\|\s*/g, '||');
+    source = source.replace(/\s*\&\&\s*/g, '&&');
+    source = source.replace(/\s*\^\^\s*/g, '^^');
+    source = source.replace(/\s*\?\s*/g, '?');
+    source = source.replace(/\s*\:\s*/g, ':');
+    source = source.replace(/\s*\+=\s*/g, '+=');
+    source = source.replace(/\s*\-=\s*/g, '-=');
+    source = source.replace(/\s*\*=\s*/g, '*=');
+    source = source.replace(/\s*\/=\s*/g, '/=');
+    source = source.replace(/\s*\+\s*/g, '+');
+    source = source.replace(/\s*\-\s*/g, '-');
+    source = source.replace(/\s*\/\s*/g, '/');
+    source = source.replace(/\s*\*\s*/g, '*');
+    source = source.replace(/\s*\(\s*/g, '(');
+    source = source.replace(/\s*\)\s*/g, ')');
+    source = source.replace(/\s*\[\s*/g, '[');
+    source = source.replace(/\s*\]\s*/g, ']');
+    source = source.replace(/\s*!\s*/g, '!');
+    source = source.replace(/\s*\+\+\s*/g, '++');
+    source = source.replace(/\s*\-\-\s*/g, '--');
+    source = source.replace(/\s*,\s*/g, ',');
+  
+    // Collapse multiple whitespace characters into one space
+    source = source.replace(/[^\S\n]{2,}/g, ' ');
+  
+    const lines = source.split(/\n+/);
+    let formatted = [];
+  
+    lines.forEach((line) => {
+      line = line.trim();
+  
+      // No empty lines
+      if (!line) {
+        return;
+      }
+  
+      // Resolve #include directives
+      if (line.startsWith('#include ')) {
+        const fileName = line.substring(9).trim();
+        const filePath = path.resolve(path.dirname(resourcePath), fileName);
+        const data = fs.readFileSync(filePath, 'utf8');
+        line = parse(data, filePath);
+        this.addDependency(filePath);
+      }
+  
+      formatted.push(line);
+    });
+  
+    return formatted.join('\n');
+  };
 
-    // Collapse multiple newlines into one newline, and multiple spaces into one space
-    source = source.replace( /(\s*\n\s*)+/g, '\n' )
-    source = source.replace( /[^\S\n]{2,}/g, ' ' )
-
-    let lines = source.split( /;|\n/ ),
-        formatted = []
-
-    lines.forEach( line => {
-        line = line.trim()
-
-        // No empty lines
-        if ( !line ) {
-            return
-        }
-
-        // Auto semicolons
-        if (
-            !line.startsWith( '#' ) &&
-            !line.endsWith( '}' ) &&
-            !line.endsWith( '{' )
-        ) {
-            line += ';'
-        }
-
-        // Resolve #include directives
-        if ( line.startsWith( '#include ' ) ) {
-            let fileName = line.substring( 9 ).trim()
-            if ( fileName.endsWith( ';' ) ) {
-                fileName = fileName.slice( 0,-1 )
-            }
-            let filePath = path.resolve( path.dirname( p ), fileName ),
-                data = fs.readFileSync( filePath, 'utf8' )
-            line = parse( data, filePath )
-        }
-
-        formatted.push( line )
-    } )
-
-    return formatted.join( '\n' )
-}
-
-module.exports = function( source ) {
-    return 'module.exports=' + JSON.stringify( parse( source, this.resourcePath ) )
+  return `module.exports= ${JSON.stringify(parse(source, this.resourcePath))}`;
 }
